8a33ddfd3a81f38e77d9edb2195d4602eaa37db9
kuntzschi112 <kuntzschi@kuntzschi.de>
2024-10-18 23:42:17 +0200

    Update ee_token.rb

--- config/initializers/ee_token.rb
+++ config/initializers/ee_token.rb
@@ -1,7 +1,13 @@
 begin
-  data = File.read(Rails.root.join(".openproject-token.pub"))
-  key = OpenSSL::PKey::RSA.new(data)
-  OpenProject::Token.key = key
-rescue StandardError
-  warn "WARNING: Missing .openproject-token.pub key"
+  if Rails.env.production?
+    data = File.read(Rails.root.join(".openproject-token.pub"))
+    key = OpenSSL::PKey::RSA.new(data)
+    OpenProject::Token.key = key
+  else
+    warn "INFO: Using a temporary key for development environment"
+    OpenProject::Token.key = OpenSSL::PKey::RSA.generate(2048)  # Generiere einen temporären Schlüssel
+  end
+rescue StandardError => e
+  warn "WARNING: Error while loading .openproject-token.pub key - #{e.message}"
 end
+
