77d28b65f6f28e13c963303224b8573c2d0dd0e0
kuntzschi112 <kuntzschi@kuntzschi.de>
2024-10-19 12:48:05 +0200

    remove ee_token.rb

--- config/initializers/ee_token.rb
+++
@@ -1,6 +0,0 @@
-#begin
-#  OpenProject::Enterprise.token = File.read(Rails.root.join('.openproject-token.pub'))
-#rescue Errno::ENOENT
-#  # File not found, using empty token
-#  OpenProject::Enterprise.token = ''
-#end
