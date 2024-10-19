4c61a4831924286e77263619c5df83d15bcffb7e
Jannick Kuntzsch <kuntzschi@kuntzschi.de>
2024-10-19 12:44:14 +0200

    Update ee_token.rb
    commented everything out

--- config/initializers/ee_token.rb
+++ config/initializers/ee_token.rb
@@ -1,6 +1,6 @@
-begin
-  OpenProject::Enterprise.token = File.read(Rails.root.join('.openproject-token.pub'))
-rescue Errno::ENOENT
-  # File not found, using empty token
-  OpenProject::Enterprise.token = ''
-end
+#begin
+#  OpenProject::Enterprise.token = File.read(Rails.root.join('.openproject-token.pub'))
+#rescue Errno::ENOENT
+#  # File not found, using empty token
+#  OpenProject::Enterprise.token = ''
+#end
